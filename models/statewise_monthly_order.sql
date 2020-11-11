ffa7bb829b1434e77bb6d31eeac21ec193d26e91
priyamvada jaiswal <priyam.jasiwal@gmail.com>
2020-11-11 11:54:55 +0000

    1.0

--- models/statewise_monthly_order.sql
+++ models/statewise_monthly_order.sql
@@ -3,7 +3,7 @@ with statewise_monthly_order as
 select S.STATE_NAME,MonthName(o.order_purchase_timestamp) as Month,C.customer_order_id
   from Customer C
   JOIN STATE S ON c.customer_st=S.ST_CD
-  JOIN Orders O on c.customer_order_id=o.customer_order_id and YEAR(o.order_purchase_timestamp)=2017
+  JOIN Orders O on c.customer_order_id=o.customer_order_id and YEAR(o.order_purchase_timestamp)={{ var('year','2017') }}--2017
 )
 
 select * from statewise_monthly_order
