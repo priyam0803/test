9c4745945d0db5cb6c5f241b82aaa7b6cc833d52
priyam0803 <73890616+priyam0803@users.noreply.github.com>
2021-07-14 13:07:21 +0530

    Update statewise_order.sql

--- models/statewise_order.sql
+++ models/statewise_order.sql
@@ -1,12 +1,5 @@
-with statewise_order as
-(
+
 select S.STATE_NAME,C.customer_order_id
   from Customer C
-  JOIN STATE S ON c.customer_st=S.ST_CD
-  JOIN Orders O on c.customer_order_id=o.customer_order_id and YEAR(o.order_purchase_timestamp)={{ var('year','2017') }}--2017
-)
-
-select * From statewise_order
---
---dbt demo
----tredence dbt demo
+  JOIN STATE S ON c.customer_st=S.ST
+  JOIN Orders O on c.customer_order_id=o.customer_order_id and YEAR(cast(o.order_purchase_timestamp as date))={{ var('year','2017') }}--2017
