7b934ad256d0836c3b9e044a02dc15d14033ad8c
priyam0803 <73890616+priyam0803@users.noreply.github.com>
2022-05-12 18:38:15 +0530

    1.0

--- models/ephemeral/statewise_order_ephemeral.sql
+++ models/ephemeral/statewise_order_ephemeral.sql
@@ -5,4 +5,4 @@ select S.STATE_NAME,C.customer_order_id
   JOIN STATE S ON c.customer_st=S.ST_CD
   JOIN Orders O on c.customer_order_id=o.customer_order_id and YEAR(o.order_purchase_timestamp)={{ var('year','2017') }}--2017
 
---
+
