5943ff77d1121904703ef8b857bef6ccfb2be4dc
priyam0803 <73890616+priyam0803@users.noreply.github.com>
2022-05-18 21:22:07 +0530

    1.0

--- models/statewise_order.sql
+++ models/statewise_order.sql
@@ -5,3 +5,5 @@ select S.STATE_NAME,C.customer_order_id,'{{ invocation_id}}' as invocation_id
   JOIN {{ source('source_test','state')}} S ON c.customer_st=S.ST_CD
   JOIN {{ source('source_test','orders')}} O on c.customer_order_id=o.customer_order_id and YEAR(o.order_purchase_timestamp) = {{ var(order_year,2017) }}
 
+  --
+
